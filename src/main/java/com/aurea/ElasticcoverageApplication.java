6489e531b2257f572b9368462745fc66c94803e9
Andrey Artyukhov <andrey.artyukhov@constant.obninsk.ru>
2017-01-19 15:55:24 +0700

    First commit

---
+++ src/main/java/com/aurea/ElasticcoverageApplication.java
@@ -0,0 +1,12 @@
+package com.aurea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticcoverageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticcoverageApplication.class, args);
+	}
+}
